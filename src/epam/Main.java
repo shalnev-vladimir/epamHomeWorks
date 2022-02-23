150d59c32d5f1862572f9d3fccb43f30eb2bb95c
Vldmr <vladimirshalnev.qa@gmail.com>
2022-02-23 14:26:37 +0300

    first commit

---
+++ src/epam/Main.java
@@ -0,0 +1,11 @@
+package epam;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PalindromeNumberEpamHomeWork palindromeNumberEpamHomeWork = new PalindromeNumberEpamHomeWork();
+        palindromeNumberEpamHomeWork.isPalindrome(121);
+    }
+    
+}
---
+++ src/epam/PalindromeNumberEpamHomeWork.java
@@ -0,0 +1,19 @@
+package epam;
+
+import static java.lang.System.*;
+
+public class PalindromeNumberEpamHomeWork {
+
+    public void isPalindrome(int x) {
+
+        int originalXNumber = x;
+        int reversedNumber = 0;
+
+        while (x != 0) {
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x = x / 10;
+        }
+        out.println(originalXNumber == reversedNumber);
+    }
+
+}
